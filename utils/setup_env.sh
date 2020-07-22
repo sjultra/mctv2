03e2ffdc04161bcfa9beee9198be321305c66805
Madalin <mabivolan@gmail.com>
2020-07-22 11:39:26 +0300

    setup_env: Added inspec env setup

--- utils/setup_env.sh
+++ utils/setup_env.sh
@@ -11,7 +11,7 @@ PIP_URL="https://bootstrap.pypa.io/get-pip.py"
 
 function install_dependencies {
     apt update -y
-    apt install -y python3 python3-distutils wget git nano unzip curl
+    apt install -y python3 python3-distutils wget git nano unzip curl apt-transport-https ca-certificates gnupg
 
     wget "$PIP_URL" -O /tmp/get-pip.py
     python3 /tmp/get-pip.py
@@ -30,11 +30,22 @@ function prepare_python_env {
     pip install python-terraform azure.identity azure-keyvault-secrets
 }
 
+function prepare_inspec_env {
+    # Install Inspec
+    curl https://omnitruck.chef.io/install.sh | bash -s -- -P inspec
+
+    # Install GCP sdk
+    echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+    curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+    apt-get update && apt-get install google-cloud-sdk
+}
+
 function main {
     install_dependencies
 
     install_terraform
     prepare_python_env
+    prepare_inspec_env
 }
 
 main $@
